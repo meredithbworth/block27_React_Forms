a8d6474a97ace54f5007e4772ee43f28536272dc
Meredith Butterworth <meredithbutterworth@gmail.com>
2024-03-09 11:01:57 -0500

    Sign Up Component Added, seeing error for uncontrolled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import SignUpForm from './components/SignUpForm'
+import Authenticate from './components/Authenticate'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <SignUpForm />
+      <Authenticate />
     </>
   )
 }
